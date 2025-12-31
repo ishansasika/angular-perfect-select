b464969182b40e61accafce86f0e5e8a5db084c7
ishansasika <ishansasika@gmail.com>
2025-12-31 18:23:10 +0530

    feat: Add public API exports

---
+++ src/public-api.ts
@@ -0,0 +1,19 @@
+/*
+ * Public API Surface of angular-perfect-select
+ */
+
+// Main Component
+export * from './lib/components/perfect-select/perfect-select.component';
+
+// Models & Interfaces
+export * from './lib/models/select-option.interface';
+export * from './lib/models/select-events.interface';
+
+// Constants & Themes
+export * from './lib/constants/themes.constant';
+
+// Animations
+export * from './lib/animations/select.animations';
+
+// Directives
+export * from './lib/directives/click-outside.directive';
