13d21e13e1ca1d4714da42752423f4de06ef77e6
ishansasika <ishansasika@gmail.com>
2026-01-06 22:55:28 +0530

    feat: Export new validation types in public API

--- src/public-api.ts
+++ src/public-api.ts
@@ -8,6 +8,7 @@ export * from './lib/components/perfect-select/perfect-select.component';
 // Models & Interfaces
 export * from './lib/models/select-option.interface';
 export * from './lib/models/select-events.interface';
+export * from './lib/models/validation.types';
 
 // Constants & Themes
 export * from './lib/constants/themes.constant';
